cfb95420536a166e1677e78af7d57fee2f06a0e6
Wesam Al-Masri <mr0virus@gmail.com>
2021-02-09 12:08:25 +0200

    convert all javasctipt statements to functions so I can reuse it easly

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,25 @@
-if (userName === ""){
-    alert("We will not contact you without entering your name.")
+function getUserName() {
+    var userName = prompt("Please enter your name");
+
+    if (userName === ""){
+        alert("We cannot contact you without entering your name.")
+    }
+
+    return userName;
+}
+
+function adminPageLink(userName) {
+    var admin;
+    console.log("user name: ", userName, typeof(userName))
+    if(userName != null){
+        if(userName === 'wesam'){
+            admin = true;
+        } else {
+            admin = false;
+        }
+    }
+
+    if(admin){
+        document.write('<li><a href="admin.html">Admin Page</a></li>')
+    }
 }
